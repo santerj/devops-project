36811ed832fbd7fa4772bb425f675c027ee719a7
Santeri Suomala <santeri.suomala@outlook.com>
2022-11-13 14:51:17 +0200

    fixed imed env vars

--- imed/src/app.py
+++ imed/src/app.py
@@ -12,8 +12,10 @@ logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 RABBITMQ_HOST = os.environ.get('RABBITMQ_HOST')
 RABBITMQ_USER = os.environ.get('RABBITMQ_USER')
 RABBITMQ_PASS = os.environ.get('RABBITMQ_PASS')
-SUB_TOPIC = os.environ.get('SUB_TOPIC')
-PUB_TOPIC = os.environ.get('PUB_TOPIC')
+EXCHANGE = os.environ.get('EXCHANGE')
+QUEUE = os.environ.get('QUEUE')
+ROUTING_KEY_READ = os.environ.get('ROUTING_KEY_READ')
+ROUTING_KEY_WRITE = os.environ.get('ROUTING_KEY_WRITE')
 
 
 def main():
@@ -21,45 +23,22 @@ def main():
     pollRabbitmqReadiness(host=RABBITMQ_HOST)
     conn = initRabbitmqConnection(RABBITMQ_HOST, RABBITMQ_USER, RABBITMQ_PASS)
     channel = conn.channel()
-    channel.exchange_declare(exchange="compse140", exchange_type="topic")
-    channel.queue_declare(queue="imed_queue", exclusive=True)
-    channel.queue_bind(queue="imed_queue", exchange="compse140",routing_key="o")
-    channel.basic_consume(queue="imed_queue", auto_ack=True, on_message_callback=callback)
+    channel.exchange_declare(exchange=EXCHANGE, exchange_type="topic")
+    channel.queue_declare(queue=QUEUE, exclusive=True)
+    channel.queue_bind(queue=QUEUE, exchange=EXCHANGE,routing_key=ROUTING_KEY_READ)
+    channel.basic_consume(queue=QUEUE, auto_ack=True, on_message_callback=callback)
     channel.start_consuming()
     
 def callback(channel: pika.channel.Channel, method: pika.spec.Basic.Deliver,
         properties: pika.spec.BasicProperties, body: bytes):
     """Send message to $PUB_TOPIC upon receiving message from $SUB_TOPIC."""
-    logging.info(f"Received message")
+    logging.info(f"Received message from {EXCHANGE}.{ROUTING_KEY_READ}")
     bodyAsString = body.decode()
     msg = f"Got {bodyAsString}"
-    channel.basic_publish(exchange='compse140', routing_key="i", body=msg,
+    channel.basic_publish(exchange=EXCHANGE, routing_key=ROUTING_KEY_WRITE, body=msg,
                                 properties=pika.BasicProperties(content_type="text/plain"))
-    logging.info(f"Published message")
+    logging.info(f"Published message to {EXCHANGE}.{ROUTING_KEY_WRITE}")
     time.sleep(1)
-
-
-
-    # subChannel = conn.channel()
-    # pubChannel = conn.channel()
-    # subChannel.queue_declare(queue=SUB_TOPIC)
-    # pubChannel.queue_declare(queue=PUB_TOPIC)
-    # callback = generateCallback(pubChannel, conn)
-    # subChannel.basic_consume(queue=SUB_TOPIC, auto_ack=True, on_message_callback=callback)
-    # subChannel.start_consuming()
-
-#def generateCallback(channel: pika.channel.Channel, conn: pika.BlockingConnection):
-#    """Wraps actual consumer callback in order to inject extra arguments"""
-#    def callback(channel: pika.channel.Channel, method: pika.spec.Basic.Deliver,
-#            properties: pika.spec.BasicProperties, body: bytes):
-#        """Send message to $PUB_TOPIC upon receiving message from $SUB_TOPIC."""
-#        logging.info(f"Received message from {SUB_TOPIC}")
-#        bodyAsString = body.decode()
-#        channel.basic_publish(exchange='', routing_key=PUB_TOPIC, body=f'Got {bodyAsString}',
-#                                  properties=pika.BasicProperties(content_type="text/plain"))
-#        logging.info(f"Published message to {PUB_TOPIC}")
-#        conn.sleep(1)
-#    return callback
 
 def pollRabbitmqReadiness(host: str) -> None:
     timeout_seconds = 5
