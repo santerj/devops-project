cf00a31d36918190117241246432e7f553891098
Santeri Suomala <santeri.suomala@outlook.com>
2022-11-13 15:39:15 +0200

    changed build contexts + Dockerfiles

---
+++ common.py
@@ -0,0 +1,27 @@
+import logging
+import time
+
+import pika
+import requests
+
+
+def pollRabbitmqReadiness(host: str) -> None:
+    timeout_seconds = 5
+    retry_seconds = 5
+    retries = 5
+    logging.info("Checking RabbitMQ readiness...")
+    for i in range(retries):
+        try:
+            r = requests.get(f"http://{host}:15692/metrics", timeout=timeout_seconds)
+            if r.status_code == 200:
+                logging.info("✅ RabbitMQ ready")
+                return
+        except requests.exceptions.ConnectionError:
+            logging.info(f"❌ RabbitMQ not ready, retrying in {retry_seconds} s ({i+1}/{retries})")
+            time.sleep(retry_seconds)
+    logging.error("RabbitMQ too slow to start")
+    exit(1)
+
+def initRabbitmqConnection(host: str, user: str, passwd: str) -> pika.BlockingConnection:
+    credentials = pika.PlainCredentials(username=user, password=passwd)
+    return pika.BlockingConnection(pika.ConnectionParameters(host=host, credentials=credentials))
