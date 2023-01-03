21f4ae1fc88bb267210f687537012cf281f526c7
Santeri Suomala <santeri.suomala@outlook.com>
2023-01-03 12:43:12 +0200

    added redis to api-gw dependencies

--- api-gw/src/app.py
+++ api-gw/src/app.py
@@ -10,3 +10,11 @@ import pika
 
 logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 
+REDIS_HOST = os.environ.get('REDIS_HOST')
+RABBITMQ_HOST = os.environ.get('RABBITMQ_HOST')
+
+
+def main():
+    pass
+
+main()
