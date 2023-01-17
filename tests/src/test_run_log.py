4f179633dcfa353f6a4ccad539e0a7db83a329d4
Santeri Suomala <santeri.suomala@outlook.com>
2023-01-17 15:35:33 +0200

    tests for /run-log

--- tests/src/test_run_log.py
+++ tests/src/test_run_log.py
@@ -2,6 +2,7 @@ import pytest
 import requests
 import redis
 
+import logging
 
 APIGW = "http://api-gw:8083"
 HTTPSERV = "http://httpserver:80"
@@ -12,4 +13,11 @@ def get_run_log():
         "Content-Type": "text/plain",
         "Accept": "text/plain"
     }
-    return requests.get(f"{APIGW}/state", headers=headers)
+    return requests.get(f"{APIGW}/run-log", headers=headers)
+
+def test_run_log_content_type(get_run_log):
+    assert "text/plain" in get_run_log.headers['Content-Type'].split(";")
+
+def test_run_log_content(get_run_log):
+    with open(file="/tmp/state-log.txt", mode="r", encoding="utf-8") as file:
+        assert file.read() == get_run_log.text
