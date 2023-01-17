8a7debb7338013373f1d5ec994bec186b5f1fa77
Santeri Suomala <santeri.suomala@outlook.com>
2023-01-17 16:10:42 +0200

    better test for /run-log

--- tests/src/test_run_log.py
+++ tests/src/test_run_log.py
@@ -19,5 +19,22 @@ def test_run_log_content_type(get_run_log):
     assert "text/plain" in get_run_log.headers['Content-Type'].split(";")
 
 def test_run_log_content(get_run_log):
-    with open(file="/tmp/state-log.txt", mode="r", encoding="utf-8") as file:
-        assert file.read() == get_run_log.text
+    # first, make a couple PUT requests to /state
+    series = ["RUNNING", "INIT", "RUNNING", "PAUSED", "RUNNING"]
+    headers = {
+        "Content-Type": "text/plain",
+        "Accept": "text/plain"
+    }
+    for state in series:
+        requests.put(f"{APIGW}/state", data={'state': state})
+
+    # next, ensure that the state has changed in the same order
+    log_parts = requests.get(f"{APIGW}/run-log", headers=headers).text.split("\n")
+    
+    # strip out empty lines
+    log_parts = [line for line in log_parts if line != ""]
+    
+    assert len(series) == len(log_parts)
+    for i, line in enumerate(log_parts):
+        state = line.split(" ")[-1]  # leave out timestamp
+        assert state == series[i]
