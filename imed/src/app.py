a75854328d3f06cf39e01eaf3f8ed14459eba6b2
Santeri Suomala <santeri.suomala@outlook.com>
2023-01-17 17:15:55 +0200

    implemented shutdown feature in intermediate

--- imed/src/app.py
+++ imed/src/app.py
@@ -3,9 +3,10 @@ import os
 import sys
 import time
 
-from common import pollRabbitmqReadiness, initRabbitmqConnection
+from common import pollRabbitmqReadiness, initRabbitmqConnection, initRedisConnection
 
 import pika
+import redis
 
 
 logging.basicConfig(stream=sys.stderr, level=logging.INFO)
@@ -18,29 +19,40 @@ EXCHANGE = os.environ.get('EXCHANGE')
 QUEUE = os.environ.get('QUEUE')
 ROUTING_KEY_READ = os.environ.get('ROUTING_KEY_READ')
 ROUTING_KEY_WRITE = os.environ.get('ROUTING_KEY_WRITE')
+REDIS_HOST = os.environ.get('REDIS_HOST')
 
 
 def main():
     # wait until RabbitMQ service is ready, init connection
     pollRabbitmqReadiness(host=RABBITMQ_HOST)
     conn = initRabbitmqConnection(RABBITMQ_HOST, RABBITMQ_USER, RABBITMQ_PASS)
+    redis = initRedisConnection(REDIS_HOST)
     
     # Setup messaging
     channel = conn.channel()
     channel.exchange_declare(exchange=EXCHANGE, exchange_type="topic")
     channel.queue_declare(queue=QUEUE, exclusive=True)
     channel.queue_bind(queue=QUEUE, exchange=EXCHANGE,routing_key=ROUTING_KEY_READ)
-    channel.basic_consume(queue=QUEUE, auto_ack=True, on_message_callback=callback)
+    channel.basic_consume(queue=QUEUE, auto_ack=True, on_message_callback=generateCallback(redis=redis))
     channel.start_consuming()
-    
-def callback(channel: pika.channel.Channel, method: pika.spec.Basic.Deliver,
-        properties: pika.spec.BasicProperties, body: bytes):
-    logging.info(f"Received message from {EXCHANGE}.{ROUTING_KEY_READ}")
-    bodyAsString = body.decode()
-    msg = f"Got {bodyAsString}"
-    channel.basic_publish(exchange=EXCHANGE, routing_key=ROUTING_KEY_WRITE, body=msg,
-                                properties=pika.BasicProperties(content_type="text/plain"))
-    logging.info(f"Published message to {EXCHANGE}.{ROUTING_KEY_WRITE}")
-    time.sleep(1)
+
+def generateCallback(redis: redis.client.Redis):
+    """Inject extra arguments to callback with currying"""
+    def callback(channel: pika.channel.Channel, method: pika.spec.Basic.Deliver,
+            properties: pika.spec.BasicProperties, body: bytes):
+        # check Redis for state
+        state = redis.get("state").decode()
+        if state == "SHUTDOWN":
+            logging.critical("Received SHUTDOWN, exiting")
+            sys.exit(0)
+        
+        logging.info(f"Received message from {EXCHANGE}.{ROUTING_KEY_READ}")
+        bodyAsString = body.decode()
+        msg = f"Got {bodyAsString}"
+        channel.basic_publish(exchange=EXCHANGE, routing_key=ROUTING_KEY_WRITE, body=msg,
+                                    properties=pika.BasicProperties(content_type="text/plain"))
+        logging.info(f"Published message to {EXCHANGE}.{ROUTING_KEY_WRITE}")
+        time.sleep(1)
+    return callback
 
 main()
